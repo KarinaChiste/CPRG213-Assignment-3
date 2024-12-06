bdb0416e3b6ecab99874ea6b0f1f9cef652dd159
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2024-12-05 22:32:16 -0700

    Contact Javascript Complete

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,22 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+let submitBtn = document.getElementById("submit-button");
+// let main = document.getElementsByTagName("main");
+
+
+function Submitting(){
+    let mainText = document.getElementById("contact-page")
+    // alert(mainText.innerHTML)
+    // mainText.style.visibility = "hidden";
+    let thankYou = document.createElement("p");
+    thankYou.innerHTML = "Thank you for your message!";
+    thankYou.style.fontSize = 24;
+    // alert(thankYou.innerHTML)
+    // document.body.append(thankYou)
+    mainText.innerHTML = thankYou.innerHTML;
+    mainText.style.textAlign = "center"
+    mainText.style.alignContent = "flex-start"
+}
+
+submitBtn.addEventListener("click", Submitting);
